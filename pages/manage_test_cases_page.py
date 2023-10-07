68bf94f510ee43117874926f27458c47e3a19cf6
Motti Weber <MottiW@cthil.com>
2023-10-07 21:30:18 +0300

    Create manage test cases Page

---
+++ pages/manage_test_cases_page.py
@@ -0,0 +1,20 @@
+import dash
+from dash import html
+
+from components.create_new_test_case_form import add_test_case_form
+from components.test_case_grid import test_case_grid
+from pages.consts import PageRoutes, PageTitles
+from pages.default_page import generate_default_layout
+
+dash.register_page(
+    __name__,
+    path=PageRoutes.MANAGE_TEST_CASES,
+    title=PageTitles.MANAGE_TEST_CASES
+)
+
+layout = generate_default_layout(
+    html.Div([
+        test_case_grid,
+        add_test_case_form,
+    ])
+)
