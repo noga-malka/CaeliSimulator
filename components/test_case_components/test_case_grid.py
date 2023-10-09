e35f464e36d80c6ca32302b49481efb940c5f98a
Motti Weber <MottiW@cthil.com>
2023-10-09 04:02:30 +0300

    Add icons

--- components/test_case_components/test_case_grid.py
+++ components/test_case_components/test_case_grid.py
@@ -1,13 +1,21 @@
 import dash_bootstrap_components
 from dash import html, callback, Input, Output
+from dash_iconify import DashIconify
 
+from assets import icons
 from components.test_case_components.consts import TestCaseGrid, TestCaseForm
 from database.database_manager import DatabaseManager
 from models.test_case import TestCase
 
 
 def generate_test_case_details(test_case: TestCase):
-    return [dash_bootstrap_components.Badge(profile_name, pill=True) for profile_name in test_case.profile_names]
+    profiles_flow = list()
+    for profile_name in test_case.profile_names:
+        profiles_flow += [
+            DashIconify(icon=icons.DOWN_ARROW, width=25),
+            dash_bootstrap_components.Badge(profile_name, pill=True, style={'margin': '5px'})
+        ]
+    return profiles_flow
 
 
 def generate_test_case_card(test_case: TestCase):
@@ -15,7 +23,8 @@ def generate_test_case_card(test_case: TestCase):
         html.Label(test_case.name, className='flex',
                    style={'justify-content': 'center', 'font-size': '20px', 'font-weight': 'bold'}),
         html.Hr(style={'margin': '5px'}),
-        html.Div(generate_test_case_details(test_case))
+        html.Div(generate_test_case_details(test_case),
+                 style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'})
     ], style={'padding': '10px', 'margin': '5px', 'height': 'fit-content', 'border': '1px solid',
               'border-radius': '10px'})
 
