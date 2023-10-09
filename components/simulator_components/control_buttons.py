97baaf001146588c535f9af63957fe2432991366
Motti Weber <MottiW@cthil.com>
2023-10-09 05:39:29 +0300

    Split buttons

--- components/simulator_components/control_buttons.py
+++ components/simulator_components/control_buttons.py
@@ -9,14 +9,17 @@ def create_control_button(title: str, icon: DashIconify):
     return dash_bootstrap_components.Button([icon, title], outline=True, color='primary', className='control-buttons')
 
 
-control_buttons = html.Div(
+control_buttons = html.Div([
     dash_bootstrap_components.ButtonGroup([
         create_control_button('Connect To Bluetooth', ControlButtonIcons.BLUETOOTH),
         create_control_button('Connect To Serial', ControlButtonIcons.SERIAL),
-        create_control_button('Select Test Case', ControlButtonIcons.SELECT_TEST_CASE),
+        create_control_button('Select Test Case', ControlButtonIcons.SELECT_TEST_CASE)
+    ]),
+    dash_bootstrap_components.ButtonGroup([
         create_control_button('On', ControlButtonIcons.ON),
         create_control_button('Run', ControlButtonIcons.PLAY),
         create_control_button('Homing', ControlButtonIcons.HOMING),
         create_control_button('Pause', ControlButtonIcons.PAUSE),
         create_control_button('Off', ControlButtonIcons.OFF),
-    ]), className='bg-secondary flex', style={'justify-content': 'center'})
+    ]),
+], className='bg-secondary flex', style={'justify-content': 'center', 'flex-direction': 'column'})
