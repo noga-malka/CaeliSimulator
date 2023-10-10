fa9165440e9a6e0f07049a5db5e05f381e42c2d4
Motti Weber <MottiW@cthil.com>
2023-10-10 18:13:36 +0300

    Add demo connection

---
+++ connections/demo_connection.py
@@ -0,0 +1,49 @@
+import random
+
+from connections.base_connection import BaseConnection
+
+
+class DemoConnection(BaseConnection):
+    def initiate(self):
+        self.device = None
+
+    def discover(self):
+        """
+        discover all near bluetooth devices
+        :return: { device name:str , mac:str}
+        """
+        self.discovered_devices = {'test': 'test'}
+
+    def connect(self, device: str) -> bool:
+        """
+        connect the given device
+        :param device: mac address of the device to connect to
+        :return: True if the connection was successful, else False
+        """
+        return True
+
+    def send(self, data: bytes):
+        print('Send: ', data)
+
+    def receive(self) -> bytes:
+        return b''
+
+    @staticmethod
+    def _random():
+        return str(random.randint(0, 10))
+
+    def receive_message(self) -> str:
+        message = [
+            'Data',
+            'Profile Index',
+            '1',
+            'Profile Run Time',
+            self._random(),
+            'Total Interval',
+            self._random(),
+            'Total Run Time',
+            self._random(),
+            'Status',
+            self._random(),
+        ]
+        return '\t'.join(message)
