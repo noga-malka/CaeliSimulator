b51e1ffe74c7358a627bbf9865db2c496881209f
Motti Weber <MottiW@cthil.com>
2023-10-13 14:56:17 +0300

    Fix commands

--- cnc/consts.py
+++ cnc/consts.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class Commands(Enum):
-    OFF: bytes = b'\x40'
-    ON: bytes = b'\x41'
-    RUN: bytes = b'\x43'
-    PAUSE: bytes = b'\x44'
+    OFF: bytes = b'\x44'
+    ON: bytes = b'\x43'
+    RUN: bytes = b'\x41'
+    PAUSE: bytes = b'\x42'
     HOMING: bytes = b'\x45'
     SYNC_PROFILES: bytes = b'\x46'
