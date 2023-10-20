a998337ce61f5df0d6bdc8fa500ce1b02fd6809f
Motti Weber <MottiW@cthil.com>
2023-10-20 15:35:02 +0300

    Add demo device

--- connections/demo_connection.py
+++ connections/demo_connection.py
@@ -12,7 +12,7 @@ class DemoConnection(BaseConnection):
         discover all near bluetooth devices
         :return: { device name:str , mac:str}
         """
-        self.discovered_devices = {}
+        self.discovered_devices = {'test': 'test'}
 
     def connect(self, device: str):
         """
