869854c0c362d70281976b32088b5c1f98a82218
Motti Weber <MottiW@cthil.com>
2023-10-20 17:42:46 +0300

    Change commands order

--- cnc/consts.py
+++ cnc/consts.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class Commands(Enum):
-    OFF: bytes = b'\x44'
-    ON: bytes = b'\x43'
     RUN: bytes = b'\x41'
     STOP: bytes = b'\x42'
+    ON: bytes = b'\x43'
+    OFF: bytes = b'\x44'
     HOMING: bytes = b'\x45'
     SEND_TEST_CASE: bytes = b'\x46'
     PAUSE_SESSION: bytes = b'\x47'
