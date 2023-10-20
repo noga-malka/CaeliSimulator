be4d4e2763d27d8aa803198fef8bfbdf3be5ba59
Motti Weber <MottiW@cthil.com>
2023-10-20 17:39:34 +0300

    Add ActiveBreathParams

--- simulator_data_manager/packet_type.py
+++ simulator_data_manager/packet_type.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
 from simulator_data_manager.packet_type_parsers.data_packet_parser import DataframePacketParser
+from simulator_data_manager.packet_type_parsers.dictionary_packet_parser import DictionaryPacketParser
 from simulator_data_manager.packet_type_parsers.profile_packet_parser import ProfilePacketParser
 
 
 class PacketType(Enum):
     Data = DataframePacketParser()
     BreathParams = ProfilePacketParser()
+    ActiveBreathParams = DictionaryPacketParser()
---
+++ simulator_data_manager/packet_type_parsers/dictionary_packet_parser.py
@@ -0,0 +1,9 @@
+from simulator_data_manager.packet_type_parsers.base_packet_parser import BasePacketParser
+
+
+class DictionaryPacketParser(BasePacketParser):
+    def __init__(self):
+        super(DictionaryPacketParser, self).__init__({})
+
+    def save(self, content: dict):
+        self.saved_data = content
