1afa5eac262137f196fc7bad7fd5713a954821bf
Motti Weber <MottiW@cthil.com>
2023-10-20 18:40:03 +0300

    Show data in graphs

--- components/simulator_components/display_live_data.py
+++ components/simulator_components/display_live_data.py
@@ -1,3 +1,4 @@
+import plotly.express as px
 from dash import html, dcc, callback, Output, Input
 from dash.exceptions import PreventUpdate
 
@@ -5,16 +6,26 @@ from components.simulator_components.consts import LiveData
 from simulator_data_manager.packet_type import PacketType
 
 
-def create_live_data_card(title, value):
+def create_live_data_card(title, content):
     return html.Div([
         html.Label(title, className='flex-center', style={'font-size': '20px', 'font-weight': 'bold'}),
         html.Hr(className='margin full-width'),
-        html.Div(value, style={'font-size': 'larger'})
+        content,
     ], className='grid-card')
 
 
+def build_graph(figure):
+    return dcc.Graph(figure=figure,
+                     style={'height': '300px'},
+                     config={
+                         'displayModeBar': False,
+                         'showAxisRangeEntryBoxes': False,
+                         'showTips': False,
+                     })
+
+
 live_data = html.Div([
-    html.Div(id=LiveData.ID, className='grid flex-center'),
+    html.Div(id=LiveData.ID, className='graph-grid flex-center'),
     dcc.Interval(id=LiveData.INTERVAL, interval=1000)
 ])
 
@@ -24,4 +35,9 @@ def update_live_data(interval):
     data = PacketType.Data.value.saved_data
     if data.empty:
         raise PreventUpdate
-    return [create_live_data_card(title, content) for title, content in PacketType.Data.value.get_live_data().items()]
+    live_cards = []
+    for column_name in data.columns:
+        figure = px.line(data[column_name], y=column_name)
+        figure.update_layout(showlegend=False)
+        live_cards.append(create_live_data_card(column_name, build_graph(figure)))
+    return live_cards
--- simulator_data_manager/packet_type_parsers/data_packet_parser.py
+++ simulator_data_manager/packet_type_parsers/data_packet_parser.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 import pandas
 
 from simulator_data_manager.packet_type_parsers.base_packet_parser import BasePacketParser
@@ -10,7 +8,11 @@ class DataframePacketParser(BasePacketParser):
         super(DataframePacketParser, self).__init__(pandas.DataFrame())
 
     def save(self, content: dict):
-        self.saved_data = self.saved_data.append(content, ignore_index=True)
+        content = {key: int(value) for key, value in content.items()}
+        new_data = self.saved_data.append(content, ignore_index=True)
+        if len(new_data) > 100:
+            new_data = new_data.iloc[1:, :]
+        self.saved_data = new_data
 
     def get_live_data(self):
         return dict(self.saved_data.iloc[-1])
