8ea4936ee7a13d1ffd106e01df5c18ae59486868
Motti Weber <MottiW@cthil.com>
2023-11-04 19:29:01 +0200

    Add unit typs

--- models/field.py
+++ models/field.py
@@ -8,7 +8,8 @@ from models.unit_type_parsers.base_parser import BaseParser
 
 
 class ProfileField:
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  unit_parser: BaseParser,
                  display_name: str,
                  icons: list,
@@ -24,7 +25,7 @@ class ProfileField:
     def generate_description(self):
         return f'{self.display_name} [{self.unit_parser.unit_type}]'
 
-    def convert_to_bytes(self, value) -> bytes:
+    def convert_to_bytes(self, value: Any) -> bytes:
         return self.unit_parser.convert_to_bytes(value)
 
     def generate_input_component(self):
@@ -36,5 +37,8 @@ class ProfileField:
             dash_bootstrap_components.Tooltip(description, target=profile_name + description,
                                               placement='top'),
             html.Div(self.icons, style={'margin-right': '5px'}, className='flex', id=profile_name + description),
-            value
+            html.Div([
+                value,
+                html.Sub(self.unit_parser.unit_type)
+            ]),
         ], className='flex', style={'align-items': 'center', 'justify-content': 'space-between'})
