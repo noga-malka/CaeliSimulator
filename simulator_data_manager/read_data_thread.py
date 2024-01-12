fc76b1436a5186166cb6c60b170d5a194f8ae9e4
Noga Malka <mail@mail.com>
2024-01-12 23:35:14 +0200

    Create different thread to crueso and simulator

--- simulator_data_manager/read_data_thread.py
+++ simulator_data_manager/read_data_thread.py
@@ -1,17 +1,9 @@
 import time
-from threading import Thread, Event
-from typing import Any
+from threading import Thread
 
 from cnc.cnc import Cnc
-from cnc.crueso_cnc import CruesoCnc
-from cnc.simulator_cnc import SimulatorCnc
 from connections.device_disconnected_exception import DeviceDisconnectedException
-from simulator_data_manager.consts import PacketHeaders
 from simulator_data_manager.packet_type_parsers.base_packet_parser import BasePacketParser
-from simulator_data_manager.packet_type_parsers.breath_parameters_packet_parser import BreathParametersPacketParser
-from simulator_data_manager.packet_type_parsers.dictionary_packet_parser import DictionaryPacketParser
-from simulator_data_manager.packet_type_parsers.float_dataframe_packet_parser import FloatDataframePacketParser
-from simulator_data_manager.packet_type_parsers.integer_dataframe_packet_parser import IntegerDataframePacketParser
 
 
 class ReadDataThread(Thread):
@@ -19,57 +11,37 @@ class ReadDataThread(Thread):
     Thread that saves the received data from the simulator
     """
 
-    def __init__(self):
+    def __init__(self, cnc_module: Cnc, packet_parsers: dict[str, BasePacketParser]):
         super().__init__()
-        self._cnc_modules = [SimulatorCnc(), CruesoCnc()]
+        self._cnc = cnc_module
         # maps between the packet header and the  parser to use to save the packet's data
-        self._packet_parsers: dict[str, BasePacketParser] = {
-            PacketHeaders.CRUESO: FloatDataframePacketParser(100),
-            PacketHeaders.DATA: IntegerDataframePacketParser(100),
-            PacketHeaders.BREATH_PARAMETERS: BreathParametersPacketParser(),
-            PacketHeaders.ACTIVE_BREATH_PARAMETERS: DictionaryPacketParser(),
-        }
+        self.packet_parsers = packet_parsers
 
     def clear_saved_data(self):
         """
         clear all parsers' saved data
         """
         time.sleep(0.1)
-        for parser in self._packet_parsers.values():
+        for parser in self.packet_parsers.values():
             parser.clear_saved_data()
 
-    def get_data(self, packet_type: str) -> Any:
-        """
-        :param packet_type: key inside self._packet_parsers
-        :return: saved data of the right packet parser
-        """
-        return self._packet_parsers[packet_type].get_saved_data()
-
-    def get_event(self, packet_type: str) -> Event:
-        """
-        :param packet_type: key inside self._packet_parsers
-        :return: Event object of the right packet parser
-        """
-        return self._packet_parsers[packet_type].get_event()
-
-    def _save_incoming_packet(self, cnc: Cnc):
+    def _save_incoming_packet(self):
         """
         extract the next packet from CNC
         if there is a valid packet parser, save the new packet, else log the unknown packet
         :raises: DeviceDisconnectedException if CNC connection is closed
         """
-        packet_type, payload = cnc.parse_incoming_packet()
-        if packet_type in self._packet_parsers:
-            self._packet_parsers[packet_type].save(payload)
+        packet_type, payload = self._cnc.parse_incoming_packet()
+        if packet_type in self.packet_parsers:
+            self.packet_parsers[packet_type].save(payload)
         else:
             print(f'Unknown packet type: {packet_type}, payload: {payload}')
 
     def run(self) -> None:
         while True:
             time.sleep(0.001)
-            for cnc in self._cnc_modules:
-                if cnc.is_connected:
-                    try:
-                        self._save_incoming_packet(cnc)
-                    except DeviceDisconnectedException:
-                        cnc.connection.disconnect()
+            if self._cnc.is_connected:
+                try:
+                    self._save_incoming_packet()
+                except DeviceDisconnectedException:
+                    self._cnc.connection.disconnect()
--- simulator_data_manager/simulator_data_manager.py
+++ simulator_data_manager/simulator_data_manager.py
@@ -1,5 +1,12 @@
 from typing import Any
 
+from cnc.crueso_cnc import CruesoCnc
+from cnc.simulator_cnc import SimulatorCnc
+from simulator_data_manager.consts import PacketHeaders
+from simulator_data_manager.packet_type_parsers.breath_parameters_packet_parser import BreathParametersPacketParser
+from simulator_data_manager.packet_type_parsers.dictionary_packet_parser import DictionaryPacketParser
+from simulator_data_manager.packet_type_parsers.float_dataframe_packet_parser import FloatDataframePacketParser
+from simulator_data_manager.packet_type_parsers.integer_dataframe_packet_parser import IntegerDataframePacketParser
 from simulator_data_manager.read_data_thread import ReadDataThread
 from singleton import Singleton
 
@@ -11,25 +18,31 @@ class SimulatorDataManager(Singleton):
     """
 
     def initiate(self):
-        self._read_data_thread = ReadDataThread()
-        self._read_data_thread.start()
+        self.simulator_thread = ReadDataThread(SimulatorCnc(),
+                                               {
+                                                   PacketHeaders.DATA: IntegerDataframePacketParser(100),
+                                                   PacketHeaders.BREATH_PARAMETERS: BreathParametersPacketParser(),
+                                                   PacketHeaders.ACTIVE_BREATH_PARAMETERS: DictionaryPacketParser(),
+                                               })
+        self.simulator_thread.start()
+        self.crueso_thread = ReadDataThread(CruesoCnc(),
+                                            {
+                                                PacketHeaders.CRUESO: FloatDataframePacketParser(100),
+                                            })
+        self.crueso_thread.start()
+
+        self._all_packet_parsers = self.simulator_thread.packet_parsers | self.crueso_thread.packet_parsers
 
     def get_data(self, packet_type: str) -> Any:
         """
         :param packet_type: key inside self._packet_parsers
         :return: saved data of the right packet parser
         """
-        return self._read_data_thread.get_data(packet_type)
+        return self._all_packet_parsers[packet_type].get_saved_data()
 
     def get_event(self, packet_type: str):
         """
         :param packet_type: key inside self._packet_parsers
         :return: Event object of the right packet parser
         """
-        return self._read_data_thread.get_event(packet_type)
-
-    def clear_saved_data(self):
-        """
-        clear all parsers' saved data
-        """
-        self._read_data_thread.clear_saved_data()
+        return self._all_packet_parsers[packet_type].get_event()
