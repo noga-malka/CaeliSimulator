28d83ef9d3aedad969af3249d21b4283fd94613a
Noga Malka <mail@mail.com>
2024-01-12 16:53:12 +0200

    Change connection_form.py to give only the simulator's serial option

--- components/simulator_components/connection_form.py
+++ components/simulator_components/connection_form.py
@@ -23,41 +23,26 @@ def build_devices_dropdown():
 
 
 connection_modal = create_modal('Connect to Simulator', ConnectionModal.ID, [
-    create_card('Select Connection Type', [
-        dcc.Dropdown(list(SimulatorCnc().connection_options.keys()),
-                     id=ConnectionModal.CONNECTION_TYPE_DROPDOWN, searchable=True,
-                     style={'width': '230px', 'margin-right': '5px'}),
-    ]),
     build_devices_dropdown(),
     dash_bootstrap_components.Button('Connect', id=ConnectionModal.CONNECT_DEVICE)
 ])
 
 
 @callback(Output(ConnectionModal.DEVICE_DROPDOWN, 'options'),
-          Input(ConnectionModal.CONNECTION_TYPE_DROPDOWN, 'value'),
-          Input(ConnectionModal.SYNC_DEVICES, 'n_clicks'),
-          prevent_initial_call=True)
-def sync_devices(connection_type: str, sync_button_clicked: int):
-    connection_options = SimulatorCnc().connection_options
-    if connection_type in connection_options:
-        return connection_options[connection_type].discover()
-    return []
+          Input(ConnectionModal.SYNC_DEVICES, 'n_clicks'))
+def sync_devices(sync_button_clicked: int):
+    return SimulatorCnc().connection.discover()
 
 
 @callback(Output(Placeholder.ID, Placeholder.Fields.CLICKS_TIMESTAMP),
           State(ConnectionModal.DEVICE_DROPDOWN, 'value'),
-          State(ConnectionModal.CONNECTION_TYPE_DROPDOWN, 'value'),
           Input(ConnectionModal.CONNECT_DEVICE, 'n_clicks'),
           prevent_initial_call=True,
           log=True)
-def connect_selected_device(device: str, connection_type: str, button_clicked: int, dash_logger: DashLogger):
+def connect_selected_device(device: str, button_clicked: int, dash_logger: DashLogger):
     cnc = SimulatorCnc()
-    if connection_type not in cnc.connection_options:
-        return ui_logger(dash_logger, 'Connection type must be selected')
     if not device:
         return ui_logger(dash_logger, 'Device must be selected')
-    cnc.set_connection(cnc.connection_options[connection_type])
-    print(cnc.connection_options[connection_type], device)
     cnc.connection.connect(device)
 
 
