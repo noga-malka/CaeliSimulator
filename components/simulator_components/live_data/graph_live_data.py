f895614236af52ec15e82ca53b3f02438b45c28e
noga-malka <malkanog@post.bgu.ac.il>
2024-01-20 18:40:20 +0200

    Add typing to graphs

--- components/simulator_components/live_data/graph_live_data.py
+++ components/simulator_components/live_data/graph_live_data.py
@@ -3,12 +3,19 @@ from dash import dcc
 from plotly import express
 
 from components.simulator_components.live_data.base_live_data import BaseLiveData
+from simulator_data_manager.packet_type_parsers.consts import CruesoKeys
+
+TYPING = {
+    CruesoKeys.PRESSURE: 'mBar',
+    CruesoKeys.TACH_B: 'RPM',
+}
 
 
 class GraphLiveData(BaseLiveData):
     def create_component(self, title: str, value: pandas.Series):
         figure = express.line(value, y=title)
-        figure.update_layout(showlegend=False)
+        yaxis_title = f'{title} [{TYPING[title]}]' if TYPING.get(title) else title
+        figure.update_layout(showlegend=False, yaxis_title=yaxis_title)
         return dcc.Graph(figure=figure,
                          style={'height': '300px'},
                          config={
