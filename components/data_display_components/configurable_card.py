62219ba802e30535cc1fcf99cf83a0a27c56a655
Motti Weber <MottiW@cthil.com>
2024-02-16 12:52:13 +0200

    improve configurable_card.py

--- components/data_display_components/configurable_card.py
+++ components/data_display_components/configurable_card.py
@@ -1,27 +1,34 @@
 import dash_bootstrap_components
 from dash import html, dcc
 
+from components.data_display_components.consts import Size, CardIdType, Display
+
 
 def generate_id(card_id: str, component_type: str):
     return {'index': card_id, 'type': component_type}
 
 
 def generate_configurable_card(card_id: str):
+    """
+    generate a card with empty content and multiple inputs to control the result content
+
+    :param card_id: unique card id to identify all sub components
+    :return: Card component with default values
+    """
     return dash_bootstrap_components.Card([
         dash_bootstrap_components.CardHeader(html.H4('display title', contentEditable="true")),
         dash_bootstrap_components.CardBody([
             html.Div([
-                dcc.Dropdown(['100%', '50%', '30%', '25%'], '100%', id=generate_id(card_id, 'size'),
+                dcc.Dropdown(Size.ALL, Size.FULL, id=generate_id(card_id, CardIdType.SIZE),
                              clearable=False, searchable=False, style={'width': '72px'}),
-                dcc.Dropdown(['Graph', 'Text', 'Timer'], 'Graph', id=generate_id(card_id, 'display'),
+                dcc.Dropdown(Display.ALL, Display.GRAPH, id=generate_id(card_id, CardIdType.DISPLAY),
                              clearable=False, searchable=False, style={'width': '77px'}),
-                dcc.Input(placeholder='Inputs', id=generate_id(card_id, 'input'),
-                          style={'border': '1px solid #ccc', 'border-radius': '4px', 'padding': '5px',
-                                 'width': '120px'}),
-                dcc.Input(placeholder='Typing', id=generate_id(card_id, 'typing'),
-                          style={'border': '1px solid #ccc', 'border-radius': '4px', 'padding': '5px',
-                                 'width': '72px'}),
+
+                dcc.Input(placeholder=CardIdType.INPUTS, id=generate_id(card_id, CardIdType.INPUTS),
+                          className='input', style={'width': '120px'}),
+                dcc.Input(placeholder=CardIdType.TYPING, id=generate_id(card_id, CardIdType.TYPING),
+                          className='input', style={'width': '72px'}),
             ], className='align'),
-            html.Div(id=generate_id(card_id, 'content'))
+            html.Div(id=generate_id(card_id, CardIdType.CONTENT))
         ]),
-    ], id=generate_id(card_id, 'card'), style={'flex': '0 0 100%'})
+    ], id=generate_id(card_id, CardIdType.CARD), style={'flex': '0 0 100%'})
---
+++ components/data_display_components/consts.py
@@ -0,0 +1,24 @@
+class Size:
+    FULL = '100%'
+    HALF = '50%'
+    ONE_THIRD = '33%'
+    QUARTER = '25%'
+
+    ALL = [FULL, HALF, ONE_THIRD, QUARTER]
+
+
+class Display:
+    GRAPH = 'Graph'
+    TEXT = 'Text'
+    TIMER = 'Timer'
+
+    ALL = [GRAPH, TEXT, TIMER]
+
+
+class CardIdType:
+    CONTENT = 'content'
+    INPUTS = 'inputs'
+    CARD = 'card'
+    DISPLAY = 'display'
+    SIZE = 'size'
+    TYPING = 'typing'
