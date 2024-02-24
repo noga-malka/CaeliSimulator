8f62f85c9613108a10efc164b94ac08db725ec98
Motti Weber <MottiW@cthil.com>
2024-02-24 21:09:25 +0200

    Add pressure Add pressure

--- simulator_data_manager/consts.py
+++ simulator_data_manager/consts.py
@@ -1,5 +1,6 @@
 class PacketHeaders:
-    CRUESO = 'Crueso'
-    DATA = 'Data'
+    CRUESO = 'Crueso_data'
+    PRESSURE = 'Pressure_data'
+    DATA = 'Simulator_ata'
     BREATH_PARAMETERS = 'BreathParams'
     ACTIVE_BREATH_PARAMETERS = 'ActiveBreathParams'
