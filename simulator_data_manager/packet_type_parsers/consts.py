cb69c775fead015fa8914e2d1da1fa6c829b178d
Motti Weber <MottiW@cthil.com>
2024-06-15 20:12:43 +0300

    Remove unused consts

--- simulator_data_manager/packet_type_parsers/consts.py
+++ simulator_data_manager/packet_type_parsers/consts.py
@@ -1,17 +1,4 @@
 class SimulatorKeys:
     INDEX = 'Index'
     PROFILE_RUN_TIME = 'Profile RunTime'
-    TOTAL_RUN_TIME = 'Total RunTime'
     CURRENT_PROFILE = 'Current Profile'
-    TOTAL_INTERVALS = 'Total Intervals'
-    BREATH_VOLUME = 'Breath Volume'
-    BREATH_STATE = 'Breathing State'
-    CRITICAL_FLAG = 'Critical Flag'
-    SIMULATOR_STATUS = 'Simulator Status'
-
-
-class CruesoKeys:
-    PRESSURE_1 = 'Pressure1'
-    TACH_B_1 = 'TachB1'
-    PRESSURE_2 = 'Pressure2'
-    TACH_B_2 = 'TachB2'
