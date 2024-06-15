d40118ec231ba354af36893c68b2b0ae3a5ca787
Motti Weber <MottiW@cthil.com>
2024-06-15 20:38:42 +0300

    Delete io_components

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import dash
 from dash import dcc
 from dash_extensions.enrich import html, DashProxy, LogTransform
 
-from components.io_components.input_connection_buttons import io_connection_buttons
+from components.io_components.bluetooth import bluetooth_connection_buttons, bluetooth_connection_modal
+from components.io_components.serial import serial_connection_buttons, serial_connection_modal
 from components.navigation_bar import navigation_bar
 from components.placeholder import placeholder
 from components.simulator_components.consts import ProgressBar
@@ -23,7 +24,12 @@ app.layout = html.Div([
         navigation_bar,
         dcc.Store(ProgressBar.CURRENT_TEST_CASE),
         html.Div([
-            io_connection_buttons,
+            html.Div([
+                html.Div(serial_connection_buttons, style=dict(width='50%')),
+                html.Div(bluetooth_connection_buttons, style=dict(width='50%')),
+                serial_connection_modal,
+                bluetooth_connection_modal,
+            ], className='bg-secondary flex-center flex-row'),
             dash.page_container  # here we insert each page content
         ], className='full-width')
     ], className='flex', style={'height': '100vh'})])
